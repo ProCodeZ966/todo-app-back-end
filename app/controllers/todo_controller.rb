ad896925d2c209e3e013f58293326e3bfff405a5
Jeremiah Elrod <creepergeneral25@gmail.com>
2017-10-22 15:55:21 +0000

    table

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,13 +2,6 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        if params[:id] ==1
-            puts ""
-        elsif params[:id] ==2
-         elsif params[:id] ==3
-          elsif params[:id] ==4 
-           elsif params[:id] ==5
-          
-        end
+        @todo = Todo.find_by_id(params[:id])
     end
 end
