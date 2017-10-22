09486c2eedb9292730df08e2796baf780a41838a
Jeremiah Elrod <creepergeneral25@gmail.com>
2017-10-22 19:27:48 +0000

    edit added

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,7 +1,30 @@
 class TodoController < ApplicationController
     def index
+    @todos = Todo.all
     end
     def show
         @todo = Todo.find_by_id(params[:id])
+    
+    end
+    def new
+    end
+    def create
+      t = Todo.new
+      t.task_name = params['task_name']
+      t.due_date = params['due_date']
+      t.completed = false
+      t.save
+      redirect_to "/todo/show/#{ t.id }"
+    end
+    def edit
+        @todo =  Todo.find_by_id(params[:id])
+    end
+    def update
+      t = Todo.find_by_id(params[:id])
+      t.due_date = params['due_date']
+      t.task_name = params['task_name']
+      t.completed = params['completed']
+      t.save
+      redirect_to "/todo/show/#{ t.id }"
     end
 end
