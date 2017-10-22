98bd3dd6f830b6739fb7bf3418e67f20afd4ddd5
Jeremiah Elrod <creepergeneral25@gmail.com>
2017-10-22 14:20:22 +0000

    params

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,5 +2,13 @@ class TodoController < ApplicationController
     def index
     end
     def show
+        if params[:id] ==1
+            puts ""
+        elsif params[:id] ==2
+         elsif params[:id] ==3
+          elsif params[:id] ==4 
+           elsif params[:id] ==5
+          
+        end
     end
 end
